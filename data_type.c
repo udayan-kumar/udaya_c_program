62e5362efd91afa4616e8b1072c069d1f523423f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-12 18:37:57 +0530

    data type

---
+++ data_type.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+    // datatype variablename = value;
+
+    // INTGER
+    int var;  // decleration of variable
+    var = 30; // intialisation
+    long num = 20;
+
+    // CHARACTER
+    char ch = 'a';
+
+    // DECIMAL NUMBER
+    float f = 12.4;
+    double d = 12.4;
+
+// specifier value
+    printf("integer = %d\n" , var);
+    printf("long = %l\n" , num);
+    printf("char = %c\n" , ch);
+    printf("float = %f\n" , f);
+    printf("double = %lf\n" , d);
+
+    return 0;
+}
