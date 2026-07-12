a6fe2c9d579698ce94a8c8ba0834c093528dccaf
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-12 18:51:05 +0530

    escape sequence

---
+++ escape_sequence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+   
+    int var = 30;  
+ 
+// printing int two double ""
+    printf("name \"udayan singh\"");
+
+// for new line
+    printf("\n");
+
+// giving space ;; tab
+    printf("integer\t %d\n" , var);  
+    
+// printing percentage
+    printf("percentage = %%\n");
+
+    return 0;
+}
