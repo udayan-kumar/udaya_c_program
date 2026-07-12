7ab410203db74a3bc1342e3e009a45cffdd2012f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-12 19:08:27 +0530

    arithmetic operation

---
+++ airathmetic_operation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 8;
+
+    printf("sum of %d and %d = %d\n" , a , b, a+b);
+    printf("difference of %d and %d = %d\n" , a , b, a-b);
+    printf("multiplication of %d and %d = %d\n" , a , b, a*b);
+    printf("division of %d and %d = %d\n" , a , b, a/b);
+    
+    return 0;
+}
