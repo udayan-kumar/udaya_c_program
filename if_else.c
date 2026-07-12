acd90ca731e0f690d297532638f1ecb6288f7ceb
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-12 20:52:33 +0530

    if else

---
+++ if_else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a = 90;
+    if(a>30){
+        printf("udayan singh");
+        
+    }
+    else {
+            printf("mannu");
+        }
+        return 0;
+}
+2:
