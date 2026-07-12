277ec5b46324e5002f59c3265e4dc79841065019
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-12 23:43:14 +0530

    loop

--- if_else.c
+++ if_else.c
@@ -9,5 +9,5 @@ int main(){
     else {
             printf("mannu");
         }
-        return 0;
+        return 0;  
 }
---
+++ loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    // for(int i = 0; i<10;i++){
+    //     printf("udayan singh\n");
+    // }
+
+    int i = 0;
+    // while(i<10){
+    //     printf("udayan singh\n");
+    //     i++;
+    // }
+
+    do{
+        printf("udayan singh\n");
+        i++;
+    }while(i<10);
+    
+    return 0;
+}
