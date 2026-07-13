1bba067560ae373c579923995b26ca269138e14e
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-13 19:39:23 +0530

    reverse the array

--- array.c
+++ array.c
@@ -4,11 +4,25 @@ int main(){
     int arr[] = {10,20,30,40};
     
     int length = sizeof(arr)/sizeof(arr[0]);
-  for(int i = 0; i<length;i++){
-    printf("%d " , arr[i]);
-  }
+
+//   for(int i = 0; i<length;i++){
+//     printf("%d " , arr[i]);
+//   }
+
+    int low = 0;
+    int high = length-1;
+    while(low<high){
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+        low++;
+        high--;
+    }
+
+    for(int i = 0; i<length;i++){
+        printf("%d " , arr[i]);
+    }
 
     return 0;
-
 
 }
