09c69ab1e36b06769bf9207d961b819e30100fd8
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-14 20:14:23 +0530

    file handling

--- file_handling.c
+++ file_handling.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
 int main(){
-    FILE *fptr = fopen("temp.text" , "read");
+    // FILE *fptr = fopen("temp.text" , "w");
+    FILE *fptr = fopen("temp.text" , "r");
+
+    char name[30];
+    int age;
 
     if(fptr==NULL){
         printf("no such file exist\n");
         return 0;
     }
-    printf("file exist\n");
+    // fputc('a' , fptr);  // for witing
+    // fputc('p' , fptr);
+    // fputc('p' , fptr);
+    // fputc('l' , fptr);
+    // fputc('e' , fptr);
+
+    // fputs("mannu sinh" , fptr);
+
+    // fprintf(fptr , "%s %d" , "udayan singh" , 20);
+
+    // fscanf(fptr , "%s %d" , name , &age);
+    // printf("%s %d\n" , name , age);
+    
+   fgets(name , 100 , fptr);
+   printf("%s %d\n" , name , age);
+   fclose(fptr);
+    return 0;
 }
-9:39
